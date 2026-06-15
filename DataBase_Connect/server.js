c2ee7de20ab14273d4cf8465973dfe334c10aece
ALIARSHAD123 <muhammedaliarshad788@gmail.com>
2026-06-15 09:50:45 +0500

    Database Connected

---
+++ DataBase_Connect/server.js
@@ -0,0 +1,12 @@
+const app = require("./src/app");
+const dns = require("dns");
+
+const connectDB = require("./src/db/db");
+
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
+
+connectDB();
+
+app.listen(3000, () => {
+  console.log("server is running on port 3000");
+});
